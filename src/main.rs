54bfe9cedca02d16da2070336a4c4e550c98f940
alegottu <alexgottuso@gmail.com>
2023-07-09 20:35:17 -0700

    added rewards functionality

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use serde::{Serialize, Deserialize};
 struct Task
 {
     pub message_id: u64, // The message to react to to gain points
-    pub value: u8 // How many points the task is worth
+    pub value: u16 // How many points the task is worth
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,18 +27,31 @@ struct User
     pub points: u64 // Accumulated points for this user
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Reward
+{
+    pub message_id: u64, // The message to react to buy this reward
+    pub cost: u16 // How many points the reward costs
+}
+
 const TASKS_FOLDER: &str = "/home/alegottu/Projects/Rust/discord-luh-bot/res/tasks";
 const USERS_FOLDER: &str = "/home/alegottu/Projects/Rust/discord-luh-bot/res/users";
+const REWARDS_FOLDER: &str = "/home/alegottu/Projects/Rust/discord-luh-bot/res/rewards";
 
 const TASKS_CHANNEL: u64 = 1100541860172271697;
+const REWARDS_CHANNEL: u64 = 1100541860172271697; // INNACURATE CHANGE LATER
+
 
 const HELP: &str = "!help";
-const ADD_TASK: &str = "!add";
-//const BUY_ITEM: &str = "!buy";   
+const ADD_TASK: &str = "!add task";
+const ADD_REWARD: &str = "!add reward";
+const CHECK_BALANCE: &str = "!balance";
+// const UNDO: &str = "!undo";
+// Would require table of past user actions
 
-const HELP_MESSAGE: &str = "!add <task> adds a task";
+const HELP_MESSAGE: &str = "!add <task_message_id> <point_value> adds a task\n!balance checks your point balance";
 
-fn load_tasks(tasks: &mut HashMap<u64, u8>) -> io::Result<()>
+fn load_tasks(tasks: &mut HashMap<u64, u16>) -> io::Result<()>
 {
     for entry in fs::read_dir(TASKS_FOLDER)?
     {
@@ -64,6 +77,21 @@ fn load_users(users: &mut HashMap<u64, u64>) -> io::Result<()>
     }
 
     Ok(())
+}
+
+fn load_rewards(rewards: &mut HashMap<u64, u16>) -> io::Result<()>
+{
+    for entry in fs::read_dir(REWARDS_FOLDER)?
+    {
+        let entry = entry?;
+        let file = File::open(entry.path())?;
+        let reader = BufReader::new(file);
+        let reward: Reward = serde_json::from_reader(reader)?;
+        rewards.insert(reward.message_id, reward.cost);
+    }
+
+    Ok(())
+
 }
 
 // Functions to save JSON files
@@ -107,11 +135,20 @@ fn update_user(user_id: u64, points: u64) -> io::Result<()>
     Ok(()) 
 }
 
+fn save_reward(reward: Reward, reward_num: usize) -> io::Result<()>
+{
+    let text = serde_json::to_string(&reward)?;
+    fs::write(REWARDS_FOLDER.to_owned() + "/" + &reward_num.to_string(), text)?;
+
+    Ok(())
+}
+
 // HashMap must be in Arcs to be allocated on the heap
 struct Handler
 {
-    tasks: Arc<Mutex<HashMap<u64, u8>>>,
-    users: Arc<Mutex<HashMap<u64, u64>>>
+    tasks: Arc<Mutex<HashMap<u64, u16>>>,
+    users: Arc<Mutex<HashMap<u64, u64>>>,
+    rewards: Arc<Mutex<HashMap<u64, u16>>>
 }
 
 #[async_trait]
@@ -135,22 +172,55 @@ impl EventHandler for Handler
                     println!("Error sending message: {:?}", why);
                 }
             },
-            ADD_TASK => // Command takes the form !add <message_id> <point value>
+            CHECK_BALANCE =>
             {
-                let mut command_split = msg.content.split(' '); 
-                command_split.next();
-                let message_id: u64 = command_split.next().expect("Task message ID not provided")
-                    .parse().expect("Invalid argument type provided for message ID");
-                let value: u8 = command_split.next().expect("Task point value not provided")
-                    .parse().expect("Invalid arugment type provided for point value");
-                let task = Task { message_id: message_id, value: value }; 
+                let users_alloc = Arc::clone(&self.users);
+                let users = users_alloc.lock().unwrap();
+                let points = users[&msg.author.id.0].to_string();
+                let message_to_send = format!("{}{}{}", "You have ", points, " total");
 
-                // Later secure that the message id provided is in the tasks channel
-                let tasks_alloc = Arc::clone(&self.tasks);
-                let tasks = tasks_alloc.lock().unwrap(); 
-                save_task(task, tasks.len()).expect("Unable to save task file");
+                msg.channel_id.say(&ctx.http, message_to_send);
+                // Figure out way to poll above
             },
-            &_ => println!("Invalid command")
+            &_ =>
+            {
+                if message.contains(ADD_TASK)
+                {
+                    // Command takes the form !add task <message_id> <point value>
+                    let mut command_split = msg.content.split(' '); 
+                    command_split.nth(1);
+                    let message_id: u64 = command_split.next().expect("Task message ID not provided")
+                        .parse().expect("Invalid argument type provided for message ID");
+                    let value: u16 = command_split.next().expect("Task point value not provided")
+                        .parse().expect("Invalid arugment type provided for point value");
+                    let task = Task { message_id: message_id, value: value }; 
+
+                    // Later secure that the message id provided is in the tasks channel
+                    let tasks_alloc = Arc::clone(&self.tasks);
+                    let tasks = tasks_alloc.lock().unwrap(); 
+                    save_task(task, tasks.len()).expect("Unable to save task file");
+                }
+                else if message.contains(ADD_REWARD) // Possibly to try to merge some of this with above case, command split the same
+                {
+                    // Command takes the form !add reward <message_id> <cost>
+                    let mut command_split = msg.content.split(' '); 
+                    command_split.nth(1);
+                    let message_id: u64 = command_split.next().expect("Reward message ID not provided")
+                        .parse().expect("Invalid argument type provided for message ID");
+                    let cost: u16 = command_split.next().expect("Reward cost not provided")
+                        .parse().expect("Invalid arugment type provided for cost");
+                    let reward = Reward { message_id: message_id, cost: cost };
+
+                    // Later secure that the message id provided is in the tasks channel
+                    let rewards_alloc = Arc::clone(&self.rewards);
+                    let rewards = rewards_alloc.lock().unwrap(); 
+                    save_reward(reward, rewards.len()).expect("Unable to save reward file");
+                }
+                else 
+                {
+                    println!("Invalid command");
+                }
+            }
         }
     }
 
@@ -171,7 +241,7 @@ impl EventHandler for Handler
                 let points = users.entry(user_id)
                     .or_insert(0); 
 
-                let amount: u8 = 
+                let amount: u16 = 
                     if tasks.contains_key(message_id)
                     {
                         *tasks.get(message_id).unwrap()
@@ -194,6 +264,11 @@ impl EventHandler for Handler
                     save_user(user, users.len() - 1)
                         .expect("Failed to create user file");
                 }
+                // Remove reaction at end of process; any reaction can be used
+            }
+            else if reaction.channel_id == REWARDS_CHANNEL
+            {
+                todo!();
             }
         }
     }
@@ -201,10 +276,12 @@ impl EventHandler for Handler
 
 #[tokio::main]
 async fn main() {
-    let mut tasks: HashMap<u64, u8> = HashMap::new();
+    let mut tasks: HashMap<u64, u16> = HashMap::new();
     let mut users: HashMap<u64, u64> = HashMap::new();
+    let mut rewards: HashMap<u64, u16> = HashMap::new();
     load_tasks(&mut tasks).expect("Could not load tasks");
     load_users(&mut users).expect("Could not load users");
+    load_rewards(&mut rewards).expect("Could not load rewards");
 
     let token = env::var("TOKEN")
         .expect("Expected a token in the environment");
@@ -216,7 +293,8 @@ async fn main() {
     let handler = Handler
     {
         tasks: Arc::new(Mutex::new(tasks)),
-        users: Arc::new(Mutex::new(users))
+        users: Arc::new(Mutex::new(users)),
+        rewards: Arc::new(Mutex::new(rewards))
     };
     let mut client = Client::builder(&token, intents)
         .event_handler(handler).await.expect("Error creating client");
